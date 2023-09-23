64d8d67a8e631a0927c285a6b01e32ea4e3c202e
manhnguyen1998 <nguyen.huu.manh@classi.jp>
2023-09-23 17:09:19 +0900

    add chrome extension client

---
+++ extension/src/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const api = "https://httpbin.org/get";
+
+const form = document.querySelector(".form-data");
+
+const country = document.querySelector(".country-name");
+
+const searchForCountry = async countryName => {
+  try {
+    console.log(countryName)
+    // const response = await axios.get(`${api}/${countryName}`);
+    const response = await axios.get(`${api}`);
+    console.log(response)
+  } catch (error) {
+    errors.textContent = "we have no dataaaaaa"
+  }
+}
+
+const handleSubmit = async e => {
+  console.log("aaaa")
+  e.preventDefault();
+  searchForCountry(country.value);
+  console.log(country.value);
+};
+
+form.addEventListener("submit", e => handleSubmit(e));
