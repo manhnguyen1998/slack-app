8c1e0c188421e0c92f0309ab4f2b085a15fa8089
manhnguyen1998 <nguyen.huu.manh@classi.jp>
2023-09-23 12:42:44 +0900

    post message to channel

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 
 func main() {
 	api := slack.New(os.Getenv("TOKEN"))
-	user, err := api.GetUserInfo(os.Getenv("USER_ID"))
+	// user, err := api.GetUserInfo(os.Getenv("USER_ID"))
+	// options := slack.MsgOption()
+	res1, res2, err := api.PostMessage("CLMTRTE2C", slack.MsgOptionText("aaaa", false))
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return
 	}
-	fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
+	// fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
+	fmt.Printf("ID: %s, Fullname: %s", res1, res2)
 }
